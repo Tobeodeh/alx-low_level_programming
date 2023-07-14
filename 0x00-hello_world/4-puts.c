e1ed8e8c3a8bed8780229390031a551db8b1139c
Tobeodeh <tchris-odeh470@stu.ui.edu.ng>
2023-07-13 18:29:48 -0700

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * prints"Programming is like building a multilingual puzzle
+ * Returms 0 if code is properly executed
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * prints out the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Returns 0 if code  exts properly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
