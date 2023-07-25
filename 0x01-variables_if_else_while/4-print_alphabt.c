7f2079fb13e85b73430a0446a97cd265690095f3
Tobeodeh <tchris-odeh470@stu.ui.edu.ng>
2023-07-25 15:03:31 -0700

    first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,except e and q
+ * followed by a new line
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+        char ch = 'a';
+
+        while (ch <= 'z')
+        {
+                if (ch != 'e' && ch != 'q')
+                {
+                        putchar(ch);
+                }
+                ch++;
+        }
+        putchar('\n')
+~                                                                                                                                                    
+~                                                                                                                                                    
+~                                     
+}
