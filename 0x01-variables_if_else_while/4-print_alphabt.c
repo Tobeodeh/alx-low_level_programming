c3255c019704507e6826dc753a1ca2f898bf9535
Tobeodeh <tchris-odeh470@stu.ui.edu.ng>
2023-07-26 05:44:11 -0700

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,18 +7,16 @@
  */
 int main(void)
 {
-        char ch = 'a';
+	char ch = 'a';
 
-        while (ch <= 'z')
-        {
-                if (ch != 'e' && ch != 'q')
-                {
-                        putchar(ch);
-                }
-                ch++;
-        }
-        putchar('\n')
-~                                                                                                                                                    
-~                                                                                                                                                    
-~                                     
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
