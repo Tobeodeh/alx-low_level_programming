ed7ca6b532de5c230d593554cbc1d781cc8fb11a
Tobeodeh <tchris-odeh470@stu.ui.edu.ng>
2023-07-28 00:42:30 -0700

    correction

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,11 @@
 */
 int main(void)
 {
-	int number;
-
-	for (number = 0; number < 10;number++)
-	printf("%i", number);
+	int num;
+ 
+	for (num = 0; num < 10; num++)
+	printf("%i", num);
 	putchar('\n');
 
 	return (0);
-}
+}}
