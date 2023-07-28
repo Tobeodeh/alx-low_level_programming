7c4bd6e27b308b2fe51160178eaa5bf0b386083c
Tobeodeh <tchris-odeh470@stu.ui.edu.ng>
2023-07-28 00:50:43 -0700

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Print all single digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	printf("%i", num);
+	putchar('\n');
+
+	return (0);
+}
